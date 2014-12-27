a5dfdbc5ea9cb13fb802eccb2ccc1c48ac2fb454
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2014-12-27 11:34:19 +0530

    Cors Corrected

--- config/cors.js
+++ config/cors.js
@@ -4,7 +4,7 @@ module.exports = function(app){
 
   var cors = require('cors');
 
-  var whitelist = ['http://localhost:1440', 'http://www.cloudboost.io'];
+  var whitelist = ['http://localhost:1440', 'http://www.cloudboost.io','http://cloudboost.io'];
   var corsOptions = {
     origin: function(origin, callback){
       var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
