e15811db8c85a1ddd7430cd38b8995f82afeb025
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-01 19:02:21 +0530

    Production Ready

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,12 @@
 module.exports = {
 	mandrill : 'qiDZR_J-NKKKfg6ieTPSYw',
-	cors : ['http://localhost:1440','http://localhost:1444', 'http://www.cloudboost.io','http://cloudboost.io'],
+	cors : ['http://localhost:1440','http://localhost:1444','https://www.cloudboost.io', 'http://www.cloudboost.io','http://dashboard.cloudboost.io','https://dashboard.cloudboost.io','http://cloudboost.io','https://cloudboost.io'],
 	devDb : 'mongodb://CloudBoost:WhiteHouse123@ds030817.mongolab.com:30817/CloudBoostDevDB',
-	productionDb : 'mongodb://CloudBoost:WhiteHouse123@ds041177.mongolab.com:41177/CloudBoostProdDB',
-	dataServiceUrl : 'http://localhost',
-	dataServiceUrlPort : 4730,
+	productionDb : 'mongodb://cbmongovm1.cloudapp.net:27017/CloudBoostFrontendServices',
+	//dataServiceUrl : 'http://localhost',
+	//dataServiceUrlPort : 4730, //fordevelopment
+	dataServiceUrl : 'http://cbdataservice.azurewebsites.net',
+	dataServiceUrlPort : 80,
 	encryptKey: "hackersbay", //for encryption of passwords
 	cbDataServicesConnectKey : 'secret-cookie'
 };
