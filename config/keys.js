7a939ce40d383f447d470b28a5b0f9633fe078ca
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-02 06:29:56 +0530

    Final Deploy

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
 	mandrill : 'qiDZR_J-NKKKfg6ieTPSYw',
 	cors : ['http://localhost:1440','http://localhost:1444','https://www.cloudboost.io', 'http://www.cloudboost.io','http://dashboard.cloudboost.io','https://dashboard.cloudboost.io','http://cloudboost.io','https://cloudboost.io'],
 	devDb : 'mongodb://CloudBoost:WhiteHouse123@ds030817.mongolab.com:30817/CloudBoostDevDB',
-	productionDb : 'mongodb://cbmongovm1.cloudapp.net:27017/CloudBoostFrontendServices',
+	productionDb : 'mongodb://CloudBoost:WhiteHouse123@ds041177.mongolab.com:41177/CloudBoostProdDB',
 	//dataServiceUrl : 'http://localhost',
 	//dataServiceUrlPort : 4730, //fordevelopment
 	dataServiceUrl : 'http://cbdataservice.azurewebsites.net',
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 ﻿var isDevelopment = process.env.PORT ? false : true;
-//var isDevelopment = true;
 var app = require('./app')(isDevelopment);
 app.set('port', process.env.PORT || 3000);
 
