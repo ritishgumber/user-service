5e3d8f07f19fbd581ed1325a8ceed8866cae8e2b
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-03 23:15:09 +0530

    Keys updated

--- config/keys.js
+++ config/keys.js
@@ -3,11 +3,8 @@ module.exports = {
 	cors : ['http://localhost:1440','http://localhost:1444','https://www.cloudboost.io', 'http://www.cloudboost.io','http://dashboard.cloudboost.io','https://dashboard.cloudboost.io','http://cloudboost.io','https://cloudboost.io'],
 	devDb : 'mongodb://CloudBoost:WhiteHouse123@ds030817.mongolab.com:30817/CloudBoostDevDB',
 	productionDb : 'mongodb://CloudBoost:WhiteHouse123@ds041177.mongolab.com:41177/CloudBoostProdDB',
-	//dataServiceUrl : 'http://localhost',
-	//dataServiceUrlPort : 4730, //fordevelopment
-	
-	dataServiceUrl : 'https://api.cloudboost.io',
-	dataServiceUrlPort : 80,
+	dataServiceUrl : global.isDevelopment ? 'http://localhost' : 'https://api.cloudboost.io',
+	dataServiceUrlPort :global.isDevelopment ? 4730 : 80,
 	encryptKey: "hackersbay", //for encryption of passwords
 	cbDataServicesConnectKey : 'secret-cookie'
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var app = require('./app')(isDevelopment);
 app.set('port', process.env.PORT || 3000);
 
+global.isDevelopment = isDevelopment;
+
 var server = app.listen(app.get('port'), function() {
 
 });
