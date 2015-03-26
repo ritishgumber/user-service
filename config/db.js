50dafcc695ee198b220405897f091baf5e01fc79
Nawaz Dhandala <hotcomputerworks@hotmail.com>
2015-03-26 10:05:07 +0530

    Bugs fixed

--- config/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var keys = require('./keys.js');
-
-module.exports = function(isDevelopment){
-
-	mongoose.connect(keys.db);
-
-	return mongoose; 
-}
--- server.js
+++
@@ -1,8 +0,0 @@
-﻿global.isDevelopment = process.env.PORT ? false : true;
-
-var app = require('./app')();
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-
-});
