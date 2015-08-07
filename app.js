29c01b6b55e7e4eea001f0752e7b306a3c7c70ad
Ranjeet <ranjeetkumar692@gmail.com>
2015-08-07 13:33:03 +0530

    More Validation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ module.exports = function(){
     var mongoose = require('./config/db.js')();
     var passport = require('passport');
     var redis = require('redis');
-    require('./config/cors.js')(app);
+    //require('./config/cors.js')(app);
     var CronJob = require('cron').CronJob;
     var Q = require('q');  
 
