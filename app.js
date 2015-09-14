0213e7ad51f4175af366f8016ea6c88e841c2b89
Vipul Maheshwari <vipul@uohyd.ac.in>
2015-09-14 13:19:09 +0530

    Cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ module.exports = function(){
             req.body = JSON.parse(req.text);
         }
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Origin', ['https://dashboard.cloudboost.io',req.headers.origin]);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
         if ('OPTIONS' === req.method) {
