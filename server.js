53e8e544971fe79c3ef7e4fa4fd3faeed25d8ca4
Nawaz Dhandala <nawazdhandala@outlook.com>
2016-08-08 21:13:01 +0530

    Process console

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ require('./app')();
 
 global.app.set('port', process.env.PORT || 3000);
 
-var server = global.app.listen(global.app.get('port'), function(){	
+var server = global.app.listen(global.app.get('port'), function(){
 	console.log("");
-	console.log("CBFrontend Services runing on PORT:"+global.app.get('port'));	
+	console.log("CBFrontend Services runing on PORT:"+global.app.get('port'));
+	console.log("Process env variables");
+	console.log(process.env);	
 });
