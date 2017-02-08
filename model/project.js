38f5f14d1b5e0d642bd31df1066828ccfe50eadb
Ritish Gumber <ritishgumber@gmail.com>
2017-02-08 18:34:15 +0530

    api for updating lastActive of project db

--- model/project.js
+++ model/project.js
@@ -1,19 +1,20 @@
-module.exports = function () {
+module.exports = function() {
 
-	var Schema = global.mongoose.Schema;
-	//console.log("Schema : "+Schema);
-	var projectSchema = new Schema({
-		name: String,
-		appId: String,
-		_userId: String,
-		keys: Object,
-		invited: Array,
-		developers: Array,
-		planId: Number,
-		provider: Object,
-		providerProperties: Object,
-		disabled: Boolean
-	}, { collection: 'projects' });
+    var Schema = global.mongoose.Schema;
+    //console.log("Schema : "+Schema);
+    var projectSchema = new Schema({
+        name: String,
+        appId: String,
+        _userId: String,
+        keys: Object,
+        invited: Array,
+        developers: Array,
+        planId: Number,
+        provider: Object,
+        providerProperties: Object,
+        disabled: Boolean,
+        lastActive: Number
+    }, {collection: 'projects'});
 
-	return global.mongoose.model('Project', projectSchema);
+    return global.mongoose.model('Project', projectSchema);
 };
