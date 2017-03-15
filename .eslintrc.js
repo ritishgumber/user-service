4e19474bc4a9c3cb59b6f6c2605531af112a740e
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-03-15 15:32:34 +0530

    Changed eslint configuration to support node env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
     "env": {
+      "node": true,
+      "browser": true
     },
     "extends": "eslint:recommended",
     "rules": {
         "semi": [
             "error",
             "always"
+        ],
+        "no-console": 0,
+        "no-unused-vars": [
+            "error", 
+            { 
+                "args": "none" 
+            }
         ]
     }
 };
