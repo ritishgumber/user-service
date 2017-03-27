2ce3d31bc096389715adb5a5bd4274222af142fd
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-03-27 10:26:53 +0530

    Auth-should register a user

---
+++ test/util/util.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  makeString: function() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  },
+
+  makeEmail: function() {
+    return this.makeString() + '@sample.com';
+  }
+
+};
