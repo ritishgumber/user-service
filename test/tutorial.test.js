767df31067489ddc145455b7abff737f86cfa1b0
Ankit Suvarna <ankitsuvarna11@gmail.com>
2017-04-02 23:25:39 +0530

    Tutorial Api

--- test/tutorial.test.js
+++ test/tutorial.test.js
@@ -2,7 +2,13 @@ describe('Tutorial', function() {
 
 	// 'get /tutorial'
 	it('should return a list of tutorial', function(done) {
-		done();
+		request
+			.get('/tutorial')
+			.end(function(err, res) {
+				expect(res).to.have.status(200);
+				expect(res.body).to.be.null;
+				done();
+			})
 	});
 	
 	// 'get /tutorial/:id'
