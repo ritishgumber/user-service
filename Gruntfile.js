7d0e67abcba62eab9693975befa4c4dd859bb7bc
Ritish Gumber <ritishgumber@gmail.com>
2017-05-10 11:14:23 +0530

    card number format changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,88 +1,95 @@
 module.exports = function(grunt) {
 
-	//initConfig will take our configuration object. This specifies which tasks
-	//and plugins we want to use
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		//specify that we would like to use the uglify plugin with the build and option parameters
-		uglify: {
-			options: {
-				//places a timestamp on top of our minified file
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-			}
-		},
+    //initConfig will take our configuration object. This specifies which tasks
+    //and plugins we want to use
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //specify that we would like to use the uglify plugin with the build and option parameters
+        uglify: {
+            options: {
+                //places a timestamp on top of our minified file
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            }
+        },
 
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec',
-					require: [
-						function() {
-							mocha = require('mocha');
-							chai = require('chai');
-							expect = chai.expect;
-							chaiHttp = require('chai-http');
-							chai.use(chaiHttp);
-							URL = "http://localhost:3000";
-							request = chai.request(URL);
-							fs = require('fs');
-							util = {
-								makeString: function() {
-									var text = "";
-									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: [function() {
+                            mocha = require('mocha');
+                            chai = require('chai');
+                            expect = chai.expect;
+                            chaiHttp = require('chai-http');
+                            chai.use(chaiHttp);
+                            URL = "http://localhost:3000";
+                            request = chai.request(URL);
+                            fs = require('fs');
+                            util = {
+                                makeString: function() {
+                                    var text = "";
+                                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-									for (var i = 0; i < 5; i++)
-										text += possible.charAt(Math.floor(Math.random() * possible.length));
+                                    for (var i = 0; i < 5; i++)
+                                        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-									return text;
-								},
-								makeEmail: function() {
-									return this.makeString() + '@sample.com';
-								},
-							  generateRandomString: function() {
-							    var text = "";
-							    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                                    return text;
+                                },
+                                makeEmail: function() {
+                                    return this.makeString() + '@sample.com';
+                                },
+                                generateRandomString: function() {
+                                    var text = "";
+                                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-							    for (var i = 0; i < 8; i++)
-							      text += possible.charAt(Math.floor(Math.random() * possible.length));
+                                    for (var i = 0; i < 8; i++)
+                                        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-							    return text;
-							  }
-							};
-						}
-					]
-				},
-				src: ['test/**/*.js']
-			}
-		},
+                                    return text;
+                                }
+                            };
+                        }
+                    ]
+                },
+                src: ['test/**/*.js']
+            }
+        },
 
-		eslint: {
-			all: ["*.js", "**/*.js", "api/**/*.js", "!node_modules/**/*.js", "!test/**/*.js", "!Gruntfile.js"]
-		},
+        eslint: {
+            all: [
+                "*.js",
+                "**/*.js",
+                "api/**/*.js",
+                "!node_modules/**/*.js",
+                "!test/**/*.js",
+                "!Gruntfile.js"
+            ]
+        },
 
-		jsbeautifier: {
-			src: ["*.js", "**/*.js", "!node_modules/**/*.js"],
-			options: {
-				js: {
-					indentSize: 1,
-					indentChar: " ",
-					indentWithTabs: true
-				}
-			}
-		}
+        jsbeautifier: {
+            src: [
+                "*.js", "**/*.js", "!node_modules/**/*.js"
+            ],
+            options: {
+                js: {
+                    indentSize: 1,
+                    indentChar: " ",
+                    indentWithTabs: true
+                }
+            }
+        }
 
-	});
+    });
 
+    ///////////////////////////////////////////////////////////
+    ///Load the uglify and mocha plugins and set it
+    ///as the default task
+    ///////////////////////////////////////////////////////////
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-jsbeautifier");
 
-	///////////////////////////////////////////////////////////
-	///Load the uglify and mocha plugins and set it 
-	///as the default task
-	///////////////////////////////////////////////////////////
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-mocha-test');
-	grunt.loadNpmTasks("grunt-eslint");
-	grunt.loadNpmTasks("grunt-jsbeautifier");
-
-	grunt.registerTask('default', ['mochaTest', 'eslint:all']);
-	grunt.registerTask('beautify_js', ['jsbeautifier']);
+    grunt.registerTask('default', ['mochaTest', 'eslint:all']);
+    grunt.registerTask('beautify_js', ['jsbeautifier']);
 };
--- test/paymentProcess.test.js
+++ test/paymentProcess.test.js
@@ -1,112 +1,87 @@
 describe('Payment Process', function() {
-	var test_user = {
-		email: util.makeEmail(),	
-		password: util.makeString(),
-		name: util.makeString()
-	};
-	var test_card = {
-		name: 'Joe Flagster',
-		number: '4000000000000002',
-		expMonth: 03,
-		expYear: 22
-	};
-	var app_name = util.makeString();
-	var Cookie, id, appId, cardId;
+    var test_user = {
+        email: util.makeEmail(),
+        password: util.makeString(),
+        name: util.makeString()
+    };
+    var test_card = {
+        name: 'Joe Flagster',
+        number: '4000000000000002',
+        expMonth: 03,
+        expYear: 22
+    };
+    var app_name = util.makeString();
+    var Cookie,
+        id,
+        appId,
+        cardId;
 
-	before(function (done) {
-		this.timeout(0);
-		var emailVerificationCode;
-		request
-			.post('/user/signup')
-			.send(test_user)
-			.end(function(err, res) {
-				emailVerificationCode = res.body.emailVerificationCode;
-				request
-					.post('/user/activate')
-					.send({code: emailVerificationCode})
-					.end(function(err, res1) {
-						request
-							.post('/user/signin')
-							.send({email: test_user.email, password: test_user.password})
-							.end(function(err, res2) {
-								Cookie = res2.headers['set-cookie'].pop().split(';')[0];
-								id = res2.body._id;
-								request
-									.post('/app/create')
-									.set('Cookie', Cookie)
-									.send({name: app_name})
-									.end(function(err, res) {
-										appId = res.body.appId;
-										done();
-									});
-							});
-					});
-			});
-	});
+    before(function(done) {
+        this.timeout(0);
+        var emailVerificationCode;
+        request.post('/user/signup').send(test_user).end(function(err, res) {
+            emailVerificationCode = res.body.emailVerificationCode;
+            request.post('/user/activate').send({code: emailVerificationCode}).end(function(err, res1) {
+                request.post('/user/signin').send({email: test_user.email, password: test_user.password}).end(function(err, res2) {
+                    Cookie = res2.headers['set-cookie'].pop().split(';')[0];
+                    id = res2.body._id;
+                    request.post('/app/create').set('Cookie', Cookie).send({name: app_name}).end(function(err, res) {
+                        appId = res.body.appId;
+                        done();
+                    });
+                });
+            });
+        });
+    });
 
-	after(function(done) {
-		request
-			.post('/user/logout')
-			.end(function(err, res){
-				done();
-			});
-	});
-	// 'post /:appId/sale'
-	it('should make a sale with proper token', function(done) {
-		done();
-	});
+    after(function(done) {
+        request.post('/user/logout').end(function(err, res) {
+            done();
+        });
+    });
+    // 'post /:appId/sale'
+    it('should make a sale with proper token', function(done) {
+        done();
+    });
 
-	// 'post /card'
-	it('should add a card with correct details', function(done) {
-		request
-			.post('/card')
-			.set('Cookie', Cookie)
-			.send(test_card)
-			.end(function(err, res) {
-				expect(res).to.have.status(200);
-				expect(res.text).to.equal('SUCCESS');
-				done();
-			});
-	});
+    // 'post /card'
+    it('should add a card with correct details', function(done) {
+        request.post('/card').set('Cookie', Cookie).send(test_card).end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.text).to.equal('SUCCESS');
+            done();
+        });
+    });
 
-	// 'get /cards'
-	it('should get cards for the specific user', function(done) {
-		request
-			.get('/cards')
-			.set('Cookie', Cookie)
-			.end(function(err, res) {
-				var cards = res.body;
-				expect(res).to.have.status(200);
-				expect(cards[0].name).to.equal(test_card.name);
-				expect(cards[0].number).to.equal('XXXX-XXXX-XXXX-' + test_card.number.slice(-4));
-				expect(cards[0].expMonth).to.equal(test_card.expMonth);
-				expect(cards[0].expYear).to.equal(test_card.expYear);
-				cardId = cards[0].cardId;
-				done();
-			});
-	});
+    // 'get /cards'
+    it('should get cards for the specific user', function(done) {
+        request.get('/cards').set('Cookie', Cookie).end(function(err, res) {
+            var cards = res.body;
+            expect(res).to.have.status(200);
+            expect(cards[0].name).to.equal(test_card.name);
+            expect(cards[0].number).to.equal(test_card.number.slice(0, 4) + '-' + test_card.number.slice(-4));
+            expect(cards[0].expMonth).to.equal(test_card.expMonth);
+            expect(cards[0].expYear).to.equal(test_card.expYear);
+            cardId = cards[0].cardId;
+            done();
+        });
+    });
 
-	// 'delete /:appId/removecard'
-	it('should update the plan for the app', function(done) {
-		request
-			.delete('/'+ appId +'/removecard')
-			.set('Cookie', Cookie)
-			.end(function(err, res) {
-				expect(res).to.have.status(200);
-				expect(res.body.message).to.equal('Success');
-				done();
-			});
-	});
+    // 'delete /:appId/removecard'
+    it('should update the plan for the app', function(done) {
+        request.delete('/' + appId + '/removecard').set('Cookie', Cookie).end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.body.message).to.equal('Success');
+            done();
+        });
+    });
 
-	// 'delete /card/:cardId'
-	it('should delete the card with valid cardId', function(done) {
-		request
-			.delete('/card/'+ cardId)
-			.set('Cookie', Cookie)
-			.end(function(err, res) {
-				expect(res).to.have.status(200);
-				done();
-			});
-	});
+    // 'delete /card/:cardId'
+    it('should delete the card with valid cardId', function(done) {
+        request.delete('/card/' + cardId).set('Cookie', Cookie).end(function(err, res) {
+            expect(res).to.have.status(200);
+            done();
+        });
+    });
 
 });
